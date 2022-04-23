8d0baf98353d4fe349a98a8715fe23a90d39fd1d
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-04-23 22:04:21 +0200

    Create OnModelCreating

--- BookLibraryApp5.0/Data/BookLibraryDbContext.cs
+++ BookLibraryApp5.0/Data/BookLibraryDbContext.cs
@@ -16,5 +16,17 @@ namespace BookLibraryApp5._0.Data
         public DbSet<Book> Books { get; init; }
 
         public DbSet<Category> Categories { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Book>()
+                .HasOne(b => b.Category)
+                .WithMany(b => b.Books)
+                .HasForeignKey(b => b.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
