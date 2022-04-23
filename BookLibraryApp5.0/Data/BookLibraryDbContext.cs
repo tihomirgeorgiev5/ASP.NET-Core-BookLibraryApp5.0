6f4959231252509a2e0e36b5b910a008ea63a57f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-04-23 19:01:01 +0200

    Added Book class

---
+++ BookLibraryApp5.0/Data/BookLibraryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryApp5._0.Data
+{
+    public class BookLibraryDbContext : IdentityDbContext
+    {
+        public BookLibraryDbContext(DbContextOptions<BookLibraryDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BookLibraryApp5.0/Data/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryApp5._0.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        [Required]
+        [MaxLength(IdMaxLength)]
+        public string Id { get; init; }
+
+        [Required]
+        [MaxLength(BookTitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(BookAuthorMaxLength)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(BookPublisherMaxLength)]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public int Pages { get; set; }
+
+        public int Year { get; set; }
+    }
+}
