8e472b723b28f57365b19995c049c342d31d0bb0
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-04-23 20:25:11 +0200

    Added DBSet for Book class

--- BookLibraryApp5.0/Data/BookLibraryDbContext.cs
+++ BookLibraryApp5.0/Data/BookLibraryDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using static BookLibraryApp5._0.Data.DataConstants;
 
 namespace BookLibraryApp5._0.Data
 {
     public class BookLibraryDbContext : IdentityDbContext
     {
+        public DbSet<Book> Books { get; init; }
         public BookLibraryDbContext(DbContextOptions<BookLibraryDbContext> options)
             : base(options)
         {
---
+++ BookLibraryApp5.0/Data/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryApp5._0.Data.Models
+{
+    public class Category
+    {
+    }
+}
