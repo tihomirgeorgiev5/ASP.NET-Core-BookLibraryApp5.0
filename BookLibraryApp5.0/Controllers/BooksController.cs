9e61097e538eb1991aa50e127e7369dd83b0e6f4
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-04-24 00:16:12 +0200

    Create BooksController

---
+++ BookLibraryApp5.0/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using BookLibraryApp5._0.Models.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibraryApp5._0.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddBookFormModel book)
+        {
+            return View();
+        }
+    }
+}
---
+++ BookLibraryApp5.0/Models/Books/AddBookFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryApp5._0.Models.Books
+{
+    public class AddBookFormModel
+    {       
+        public string Title { get; init; }
+        
+        public string Author { get; init; }
+
+        public string Publisher { get; init; }
+       
+        public string ImageUrl { get; init; }
+    
+        public string Description { get; init; }
+
+        public int Year { get; init; }
+
+        public int CategoryId { get; init; }
+    }
+}
