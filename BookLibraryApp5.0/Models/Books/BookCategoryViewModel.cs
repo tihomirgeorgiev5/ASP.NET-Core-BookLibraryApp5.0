c7a38c4ba85b57774386ca1a4fa1d3fcd84b6d5f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-04-24 14:20:19 +0200

    Added BookCategoryViewModel

---
+++ BookLibraryApp5.0/Models/Books/BookCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryApp5._0.Models.Books
+{
+    public class BookCategoryViewModel
+    {
+        
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+    }
+    
+}
+
