0b27a8ab02aa52dbdc8dbc179db5e277466f516e
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-07 16:48:19 +0200

    Added new Sorting Model in Books

---
+++ BookLibraryApp5.0/Models/Books/AllBooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BookLibraryApp5._0.Models.Books
+{
+    public class AllBooksQueryModel
+    {
+        public IEnumerable<string> Titel { get; init; }
+        public IEnumerable<string> SearchTerm { get; init; }
+        public BookSorting Sorting { get; init; }
+        public IEnumerable<BookListingViewModel> Books { get; init; }
+
+    }
+}
+ 
---
+++ BookLibraryApp5.0/Models/Books/BookSorting.cs
@@ -0,0 +1,10 @@
+﻿ namespace BookLibraryApp5._0.Models.Books
+{
+    public enum BookSorting
+    {
+        DateCreated = 0,
+        Year = 1,
+        TitelAndAuthor = 2
+    }
+}
+ 
