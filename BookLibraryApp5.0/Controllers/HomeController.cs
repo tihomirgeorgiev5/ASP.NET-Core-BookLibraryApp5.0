a50fb799fd9940cde42d25ae964e0229f7af006a
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-07 09:16:55 +0200

    Added new functionality in HomeController

--- BookLibraryApp5.0/Controllers/HomeController.cs
+++ BookLibraryApp5.0/Controllers/HomeController.cs
@@ -1,13 +1,39 @@
-﻿using BookLibraryApp5._0.Models;
+﻿using BookLibraryApp5._0.Data;
+using BookLibraryApp5._0.Models;
+using BookLibraryApp5._0.Models.Books;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
 
 namespace BookLibraryApp5._0.Controllers
 {
     public class HomeController : Controller
     {
-       
-        public IActionResult Index() => View();
+        private readonly BookLibraryDbContext data;
+
+        public HomeController(BookLibraryDbContext data)
+            => this.data = data;
+
+        public IActionResult Index()
+        {
+            var books = this.data
+               .Books
+               .OrderByDescending(b => b.Id)
+               .Select(b => new BookListingViewModel
+               {
+                   Id = b.Id,
+                   Title = b.Title,
+                   Author = b.Author,
+                   Publisher = b.Publisher,
+                   Year = b.Year,
+                   ImageUrl = b.ImageUrl,
+                   Category = b.Category.Name
+               })
+               .Take(3)
+               .ToList();
+
+            return View(books);
+        }
           
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
